0a121ba5bb7488a14fdd49bc027e39324b5aef87
Alexei Accio <alexei.accio@gmail.com>
2018-05-10 23:48:19 +0300

    change styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 1200,
+              maxWidth: 1600,
               backgroundColor: "transparent",
               linkImagesToOriginal: false,
               showCaptions: true,
